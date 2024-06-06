00971072af5967a96389f6a1636789c058e08313
WDK-Techno <wdilshankavindra@gmail.com>
2024-06-06 11:01:53 +0530

    create oucard component

--- src/pages/excomFLow/excomLandingPage/excomLandingPage.jsx
+++ src/pages/excomFLow/excomLandingPage/excomLandingPage.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+// import React from 'react'
+import CommonButton from "../../../components/common/commonButton/commonButton";
+import OuCard from "../../../components/common/oucard/ouCard";
+
+import sbLogo from "../../../assets/logo/sb_logo.png";
 
 const ExcomLandingPage = () => {
   return (
     <div>
-      <h1 className='text-cl-primary'>ExcomLandingPage</h1>
+      <h1 className="text-cl-primary">ExcomLandingPage</h1>
+      <div className="w-25 mt-3">
+        <CommonButton text={"WDK"} />
+      </div>
+      <div className="w-25">
+        <OuCard name={"IEEE UWU SB"} type={"Student Branch"} logo={sbLogo} />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ExcomLandingPage
+export default ExcomLandingPage;
