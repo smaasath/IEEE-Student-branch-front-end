2fbdaca8285c72c90e568e7f36f25f4d2d7828d2
WDK-Techno <wdilshankavindra@gmail.com>
2024-06-06 14:36:30 +0530

    oucard list show in excom landing page

--- src/components/common/oucard/ouCard.jsx
+++ src/components/common/oucard/ouCard.jsx
@@ -2,11 +2,20 @@ import React from "react";
 
 const OuCard = ({ name, type, logo, onclick }) => {
   return (
-    <div className="card btn m-0 p-0 shadow-sm rounded-5 ">
-      <img src={logo} className="card-img-top body-bag-secondary pt-3 pb-2 px-2 align-middle rounded-top-5" alt="" />
+    <div
+      className="card btn m-0 p-0 shadow-sm rounded-5"
+      style={{ width: "100%", height: "180px" }}
+    >
+      <img
+        src={logo}
+        className="card-img-top body-bag-secondary pt-3 pb-2 px-2 align-middle rounded-top-5"
+        alt=""
+        style={{ width: '100%', height: '100px', objectFit: 'contain' }}
+      />
       <div className="card-body m-0 pt-2">
         <h4 className="card-title text-start text-uppercase mt-0 mb-1 text-wrap text-cl-primary fw-bolder">
-            {name}</h4>
+          IEEE UWU {name}
+        </h4>
         <h6 className="card-text text-start text-capitalize text-wrap text-cl-primary">
           {type}
         </h6>
--- src/pages/excomFLow/excomLandingPage/excomLandingPage.jsx
+++ src/pages/excomFLow/excomLandingPage/excomLandingPage.jsx
@@ -3,16 +3,56 @@ import CommonButton from "../../../components/common/commonButton/commonButton";
 import OuCard from "../../../components/common/oucard/ouCard";
 
 import sbLogo from "../../../assets/logo/sb_logo.png";
+import wieLogo from "../../../assets/logo/wie_logo.png";
+import iasLogo from "../../../assets/logo/ias_logo.png";
+import rasLogo from "../../../assets/logo/ras_logo.png";
+
+const ous = [
+  {
+    id: 0,
+    name: "SB",
+    type: "Student Branch",
+    logo: sbLogo,
+  },
+  {
+    id: 1,
+    name: "WIE",
+    type: "Affinity Group",
+    logo: wieLogo,
+  },
+  {
+    id: 2,
+    name: "RAS",
+    type: "Technical Chapter",
+    logo: rasLogo,
+  },
+  {
+    id: 3,
+    name: "IAS",
+    type: "Technical Chapter",
+    logo: iasLogo,
+  },
+  {
+    id: 4,
+    name: "CS",
+    type: "Technical Chapter",
+    logo: iasLogo,
+  },
+];
 
 const ExcomLandingPage = () => {
   return (
-    <div>
-      <h1 className="text-cl-primary">ExcomLandingPage</h1>
-      <div className="w-25 mt-3">
-        <CommonButton text={"WDK"} />
-      </div>
-      <div className="w-25">
-        <OuCard name={"IEEE UWU SB"} type={"Student Branch"} logo={sbLogo} />
+    <div className="container overflow-scroll">
+      <div className="row">
+        <h6 className="text-cl-primary text-start">Chapters</h6>
+        {ous.map((ou) => (
+          <div
+            key={ou.id}
+            className="col-10 col-sm-6 col-md-5 col-lg-3 me-0 mb-4"
+          >
+            <OuCard name={ou.name} type={ou.type} logo={ou.logo} />
+          </div>
+        ))}
       </div>
     </div>
   );
