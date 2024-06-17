be49e30386c89df0e17550092cf16b5182c377e5
Aasath <aasadh2000@gmail.com>
2024-06-17 08:22:29 +0530

    project time line design

--- src/components/common/viewSwitcher/viewSwitcher.jsx
+++ src/components/common/viewSwitcher/viewSwitcher.jsx
@@ -3,48 +3,24 @@ import "gantt-task-react/dist/index.css";
 import { ViewMode } from "gantt-task-react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const ViewSwitcher = ({ onViewModeChange, onViewListChange, isChecked }) => {
+const ViewSwitcher = ({ onViewModeChange, view }) => {
   return (
     <div className="d-flex flex-column align-items-start p-2">
       <div className="btn-group mb-2" role="group">
         <button
-          className="btn btn-primary"
-          onClick={() => onViewModeChange(ViewMode.Hour)}
-        >
-          Hour
-        </button>
-        <button
-          className="btn btn-primary"
-          onClick={() => onViewModeChange(ViewMode.QuarterDay)}
-        >
-          Quarter of Day
-        </button>
-        <button
-          className="btn btn-primary"
-          onClick={() => onViewModeChange(ViewMode.HalfDay)}
-        >
-          Half of Day
-        </button>
-        <button
-          className="btn btn-primary"
+           className={`btn ${view == ViewMode.Day ? "bg-third text-white" : "text-dark"}`}
           onClick={() => onViewModeChange(ViewMode.Day)}
         >
           Day
         </button>
         <button
-          className="btn btn-primary"
-          onClick={() => onViewModeChange(ViewMode.Week)}
-        >
-          Week
-        </button>
-        <button
-          className="btn btn-primary"
+           className={`btn ${view == ViewMode.Month ? "bg-third text-white" : "text-dark"}`}
           onClick={() => onViewModeChange(ViewMode.Month)}
         >
           Month
         </button>
         <button
-          className="btn btn-primary"
+           className={`btn ${view == ViewMode.Year ? "bg-third text-white" : "text-dark"}`}
           onClick={() => onViewModeChange(ViewMode.Year)}
         >
           Year
--- src/pages/projectFlow/timeLinePage/timeLinePage.jsx
+++ src/pages/projectFlow/timeLinePage/timeLinePage.jsx
@@ -3,64 +3,45 @@ import { Gantt, ViewMode } from 'gantt-task-react';
 import "gantt-task-react/dist/index.css";
 import { Modal, Button } from 'react-bootstrap';
 import ViewSwitcher from '../../../components/common/viewSwitcher/viewSwitcher';
+import CommonButton from '../../../components/common/commonButton/commonButton';
+
 
 const TimeLinePage = () => {
-    const getRandomColor = () => {
-        const colors = [
-            '#ff5733',
-            '#3357ff',
-            '#ff33a1',
-            '#9933ff',
-            '#ff3380',
-        ];
-
-        return colors[Math.floor(Math.random() * colors.length)];
-    };
     const [tasks] = useState([
         {
             start: new Date(2020, 1, 1),
             end: new Date(2020, 3, 2),
             name: 'Idea',
-            id: 'Task 0',
+            id: '1',
             type: 'task',
-            progress: 100,
-            styles: { progressColor: '#0E2954' },
         },
         {
             start: new Date(2020, 1, 1),
             end: new Date(2020, 3, 2),
             name: 'Idea',
-            id: 'Task 0',
+            id: '2',
             type: 'task',
-            progress: 100,
-            styles: {  progressColor: '#0E2954' },
-        }, 
+        },
         {
             start: new Date(2021, 1, 1),
             end: new Date(2022, 3, 2),
             name: 'Idea',
-            id: 'Task 0',
+            id: '3',
             type: 'task',
-            progress: 100,
-            styles: {  progressColor: '#0E2954' },
-        }, 
+        },
         {
             start: new Date(2022, 1, 1),
             end: new Date(2023, 3, 2),
             name: 'Idea',
-            id: 'Task 0',
+            id: '4',
             progress: 100,
-            type: 'task',
-            styles: {  progressColor: '#0E2954' },
-        }, 
+        },
         {
             start: new Date(2023, 1, 1),
             end: new Date(2024, 3, 2),
             name: 'Idea',
-            id: 'Task 0',
+            id: '5',
             type: 'task',
-            progress: 100,
-            styles: {  progressColor: '#0E2954' },
         },
     ]);
 
@@ -91,7 +72,7 @@ const TimeLinePage = () => {
                     const isProject = item.type === "project";
                     return (
                         <div
-                            key={item.id}
+                            key={i}
                             style={{
                                 height: rowHeight,
                                 width: "auto",
@@ -122,32 +103,58 @@ const TimeLinePage = () => {
 
     return (
         <div className="container mt-4">
-            <ViewSwitcher
-                onViewModeChange={(viewMode) => setView(viewMode)}
-            />
-            <Gantt tasks={tasks} onClick={handleTaskClick}
-                rowHeight={40}
-                todayColor="rgba(246, 246, 247, .6)"
-                timeStep={100}
-                columnWidth={80}
-                TaskListHeader={({ headerHeight }) => (
-                    <div
-                        style={{
-                            height: headerHeight,
-                                 width: "auto",
+            <div className='text-cl-primary'>Project Time line</div>
+            <div className='mt-3 d-flex justify-content-end align-items-center gap-3 flex-wrap'>
+                <div className="">
+                    <select className="form-select w-100" aria-label="Large select example">
+                        <option selected>Select Year</option>
+                        <option value="1">2024</option>
+                    </select>
+                </div>
+                <div className="">
+                    <select className="form-select w-100" aria-label="Large select example">
+                        <option selected>Select Entity</option>
+                        <option value="1">CS Chapter</option>
+                    </select>
+                </div>
+                <div>
+                    <ViewSwitcher
+                        onViewModeChange={(viewMode) => setView(viewMode)}
+                        view={view}
+                    />
+                </div>
+            </div>
+
+            <div className='mt-4'>
+                <Gantt tasks={tasks} onClick={handleTaskClick}
+                    rowHeight={40}
+                    todayColor="rgba(246, 246, 247, .6)"
+                    timeStep={100}
+                    columnWidth={80}
+                    barBackgroundColor='#00629B'
+                    barBackgroundSelectedColor='#0E2954'
+                    TaskListHeader={({ headerHeight }) => (
+                        <div
+                            style={{
+                                height: headerHeight,
+                                width: "auto",
                                 display: "flex",
                                 justifyContent: "space-between",
                                 alignItems: "center",
-                                background:  "#ffffff" ,
+                                background: "#ffffff",
                                 padding: 10,
-                        }}
-                    >
-                       <h6 className='text-third fw-bold'>Projects</h6> 
-                    </div>
-                )}
-                viewMode={view} TaskListTable={(props) => (
-                    <TaskListTable {...props} />
-                )} />
+                            }}
+                        >
+                            <h6 className='text-third fw-bold'>Projects</h6>
+                        </div>
+                    )}
+                    viewMode={view} TaskListTable={(props) => (
+                        <TaskListTable {...props} />
+                    )} />
+            </div>
+
+
+            {/* model */}
             <Modal show={modalIsOpen} onHide={handleCloseModal}>
                 <Modal.Header closeButton>
                     <Modal.Title>Task Details</Modal.Title>
@@ -155,17 +162,37 @@ const TimeLinePage = () => {
                 <Modal.Body>
                     {selectedTask && (
                         <div>
-                            <h5>{selectedTask.name}</h5>
-                            <p><strong>Start:</strong> {selectedTask.start.toDateString()}</p>
-                            <p><strong>End:</strong> {selectedTask.end.toDateString()}</p>
-                            <p><strong>Progress:</strong> {selectedTask.progress}%</p>
+                            <div className='d-flex justify-content-between align-items-center'>
+                                <div>
+                                    Start Date
+                                </div>
+                                <div className=''>
+                                    <div className="input-group input-group-sm">
+                                        <input type="date" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='mt-4 d-flex justify-content-between align-items-center'>
+                                <div>
+                                    End Date
+                                </div>
+                                <div className=''>
+                                    <div className="input-group input-group-sm">
+                                        <input type="date" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" />
+                                    </div>
+                                </div>
+                            </div>
+
                         </div>
                     )}
                 </Modal.Body>
                 <Modal.Footer>
-                    <Button variant="secondary" onClick={handleCloseModal}>
-                        Close
-                    </Button>
+                    <div>
+                        <CommonButton onClick={handleCloseModal} close={true} text={"Close"} />
+                    </div>
+                    <div>
+                        <CommonButton text={"Save"} />
+                    </div>
                 </Modal.Footer>
             </Modal>
         </div>
