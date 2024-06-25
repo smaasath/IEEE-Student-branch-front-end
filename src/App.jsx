e7ca389b5c3fcbbba9cc5f6500962171a3c218fa
Thilini Priyangika <85427990+Thilinipriyangika@users.noreply.github.com>
2024-06-26 00:38:38 +0530

    Create executiveCommitteePage

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import { Route, Routes, } from 'react-router-dom';
 import DashboardLayout from './components/layouts/dashboardLayout/dashboardLayout';
 import ExcomLandingPage from './pages/excomFLow/excomLandingPage/excomLandingPage';
+import ExecutiveCommitteePage from './pages/excomFLow/executiveCommitteePage/executiveCommitteePage';
 import SignIN from './pages/authFlow/signIn/signIn';
 import SignUp from './pages/authFlow/signUp/signUp';
 import VerifyCode from './pages/authFlow/verifyCode/verifyCode';
@@ -32,6 +33,7 @@ function App() {
         <Route path='/forgot-password/change-password' element={<ChangePassword />} />
         <Route path='/dashboard' element={<DashboardLayout />}>
           <Route path='executive-committee' element={<ExcomLandingPage />} />
+          <Route path='executiveCommitteePage' element={<ExecutiveCommitteePage />} />
           <Route path='project'>
             <Route path='' element={<ProjectLandingPage />} />
             <Route path='time-line' element={<TimeLinePage />} />
---
+++ src/pages/excomFLow/executiveCommitteePage/executiveCommitteePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const executiveCommitteePage = () => {
+  return (
+    <div>
+      <h1>Hiii</h1>
+    </div>
+  )
+}
+
+export default executiveCommitteePage
