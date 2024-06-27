c491b460815b0cc1f78692c7d1ebea8db65eee7e
Thilini Priyangika <85427990+Thilinipriyangika@users.noreply.github.com>
2024-06-28 00:01:25 +0530

    Excom Landing Page Common table  added

--- src/pages/excomFLow/excomLandingPage/excomLandingPage.jsx
+++ src/pages/excomFLow/excomLandingPage/excomLandingPage.jsx
@@ -1,213 +1,188 @@
-// import React from 'react'
-import CommonButton from "../../../components/common/commonButton/commonButton";
+import React, { useEffect, useState } from 'react';
+import CommonSearch from '../../../components/common/commonSearch/commonSearch';
+import CommonTable from '../../../components/common/commonTable/commonTable';
+import CommonPagination from '../../../components/common/commonPagination/commonPagination';
+import ProposalStatusChangeModel from '../../../components/models/proposalStatusChangeModel/proposalStatusChangeModel';
 import OuCard from "../../../components/common/oucard/ouCard";
 
 import sbLogo from "../../../assets/logo/sb_logo.png";
 import wieLogo from "../../../assets/logo/wie_logo.png";
 import iasLogo from "../../../assets/logo/ias_logo.png";
 import rasLogo from "../../../assets/logo/ras_logo.png";
-import ExcomTable from "../../../components/common/excomTable/excomTable";
-import { useEffect, useState } from "react";
 
 const ExcomLandingPage = () => {
-  const currentYear = new Date().getFullYear();
-  const [searchByName, setSearchByname] = useState("");
-  const [entityFilter, setEntityFilter] = useState("All");
-  const [termFilter, setTermFilter] = useState(currentYear);
-  const [availableTermYears, setAvailableTermYears] = useState([]);
+    const currentYear = new Date().getFullYear();
+    const [searchByName, setSearchByName] = useState("");
+    const [entityFilter, setEntityFilter] = useState("All");
+    const [termFilter, setTermFilter] = useState(currentYear);
+    const [availableTermYears, setAvailableTermYears] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [statusChangeModelShow, setStatusChangeModelShow] = useState(false);
 
-  // collect uniqe years from termYear property in deceinsing order
-  useEffect(() => {
-    const termYears = [...new Set(excomData.map((item) => item.termYear))].sort(
-      (a, b) => b - a
+    const handleCloseStatusChangeModel = () => setStatusChangeModelShow(false);
+    const handleShowStatusChangeModel = () => setStatusChangeModelShow(true);
+
+    useEffect(() => {
+        const termYears = [...new Set(tableData.map((item) => item.termYear))].sort((a, b) => b - a);
+        setAvailableTermYears(termYears);
+    }, []);
+
+    const handleSearchChange = (e) => setSearchByName(e.target.value);
+    const handleEntityChange = (e) => setEntityFilter(e.target.value);
+    const handleTermChange = (e) => setTermFilter(e.target.value);
+
+    const tableHeading = [
+        { lable: "First Name", value: "fname" },
+        { lable: "Last Name", value: "lname" },
+        { lable: "Email", value: "email" },
+        { lable: "Contact Number", value: "contactNo" },
+        { lable: "Entity", value: "entity" },
+        { lable: "Position", value: "position" },
+        { lable: "Academic Year", value: "academicYear" },
+        { lable: "Term Year", value: "termYear" },
+        { lable: "", value: "ACTION", type: ["VIEW"] },
+    ];
+
+    const tableData = [
+        {
+            id: 1,
+            fname: "Kavindra",
+            lname: "Weerasinghe",
+            email: "wdilshankavindra@gmail.com",
+            contactNo: "0774743603",
+            entity: "SB",
+            position: "Chairperson",
+            academicYear: "3rd Year",
+            termYear: "2024",
+        },
+        {
+            id: 2,
+            fname: "Sanjana",
+            lname: "Attanayake",
+            email: "sanajan@gamil.com",
+            contactNo: "0742365896",
+            entity: "SB",
+            position: "Secretary",
+            academicYear: "3rd Year",
+            termYear: "2022",
+        },
+        {
+            id: 3,
+            fname: "Kasun",
+            lname: "Janith",
+            email: "kasun@gamil.com",
+            contactNo: "0712365896",
+            entity: "IAS",
+            position: "Vice Chair",
+            academicYear: "2nd Year",
+            termYear: "2024",
+        },
+        {
+            id: 4,
+            fname: "Joshiga",
+            lname: "Ravikumar",
+            email: "joshi@gamil.com",
+            contactNo: "0712365895",
+            entity: "WIE",
+            position: "Chairperson",
+            academicYear: "3rd Year",
+            termYear: "2023",
+        },
+        {
+            id: 5,
+            fname: "Sasan",
+            lname: "Dilantha",
+            email: "sasan@gamil.com",
+            contactNo: "0712364596",
+            entity: "IAS",
+            position: "Chairperson",
+            academicYear: "3rd Year",
+            termYear: "2024",
+        },
+        {
+            id: 6,
+            fname: "Nipuna",
+            lname: "Deshan",
+            email: "nipuna@gamil.com",
+            contactNo: "0712367586",
+            entity: "IAS",
+            position: "PV Head",
+            academicYear: "3rd Year",
+            termYear: "2024",
+        },
+    ];
+
+    // Filter excomData based on search, entity, and term
+    const filteredData = tableData.filter((item) => {
+        const matchesName = item.fname.toLowerCase().includes(searchByName.toLowerCase());
+        const matchesEntity = entityFilter === "All" || item.entity === entityFilter;
+        const matchesTerm = termFilter === currentYear || item.termYear.toString() === termFilter.toString();
+
+        return matchesName && matchesEntity && matchesTerm;
+    });
+
+    return (
+        <>
+            <div className='container'>
+                <div className='text-cl-primary'>Entities</div>
+                <div className="row mt-3">
+                    {[
+                        { id: 0, name: "SB", type: "Student Branch", logo: sbLogo },
+                        { id: 1, name: "WIE", type: "Affinity Group", logo: wieLogo },
+                        { id: 2, name: "RAS", type: "Technical Chapter", logo: rasLogo },
+                        { id: 3, name: "IAS", type: "Technical Chapter", logo: iasLogo },
+                        { id: 4, name: "CS", type: "Technical Chapter", logo: iasLogo }
+                    ].map((ou) => (
+                        <div key={ou.id} className="col-10 col-sm-6 col-md-5 col-lg-3 me-0 mb-4">
+                            <OuCard name={ou.name} type={ou.type} logo={ou.logo} />
+                        </div>
+                    ))}
+                </div>
+                <div className='text-cl-primary mt-4'>Members details</div>
+                <div className='mt-3 pt-4 p-3 rounded-4 bg-white common-shadow'>
+                    <div className='d-flex justify-content-between flex-wrap align-items-center p-3'>
+                        <div>
+                            <CommonSearch primary={true} onChange={handleSearchChange} />
+                        </div>
+                        <div>
+                            <select className="form-select" aria-label="Select Entity" value={entityFilter} onChange={handleEntityChange}>
+                                <option value="All">Select Entity</option>
+                                {[
+                                    { id: 0, name: "SB", type: "Student Branch", logo: sbLogo },
+                                    { id: 1, name: "WIE", type: "Affinity Group", logo: wieLogo },
+                                    { id: 2, name: "RAS", type: "Technical Chapter", logo: rasLogo },
+                                    { id: 3, name: "IAS", type: "Technical Chapter", logo: iasLogo },
+                                    { id: 4, name: "CS", type: "Technical Chapter", logo: iasLogo }
+                                ].map((ou) => (
+                                    <option key={ou.id} value={ou.name}>
+                                        {ou.name}
+                                    </option>
+                                ))}
+                            </select>
+                        </div>
+                        <div>
+                            <select className="form-select ms-2 me-1" value={termFilter} onChange={handleTermChange}>
+                                <option value={currentYear}>Select Term</option>
+                                {availableTermYears.map((year) => (
+                                    <option key={year} value={year}>
+                                        Term {year}
+                                    </option>
+                                ))}
+                            </select>
+                        </div>
+                    </div>
+
+                    <div className='mt-3 p-3 rounded-4 bg-white d-flex flex-column justify-content-between table-container'>
+                        <CommonTable tableHeading={tableHeading} tableData={filteredData} primary={true} loading={false} editAction={(id) => { handleShowStatusChangeModel() }} />
+                        <div className='mt-4 d-flex justify-content-end'>
+                            <CommonPagination pages={10} currentPage={currentPage} setCurrentPage={setCurrentPage} />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <ProposalStatusChangeModel show={statusChangeModelShow} onHide={handleCloseStatusChangeModel} />
+        </>
     );
-    setAvailableTermYears(termYears);
-  }, []);
-
-  const handleSearchChange = (e) => {
-    setSearchByname(e.target.value);
-  };
-
-  const handleEntityChange = (e) => {
-    setEntityFilter(e.target.value);
-  };
-
-  const handleTermChange = (e) => {
-    setTermFilter(e.target.value);
-  };
-
-  return (
-    <div className="container overflow-scroll">
-      {/* Entities */}
-      <div className="row">
-        <h6 className="text-cl-primary text-start">Entities</h6>
-        {ous.map((ou) => (
-          <div
-            key={ou.id}
-            className="col-10 col-sm-6 col-md-5 col-lg-3 me-0 mb-4"
-          >
-            <OuCard name={ou.name} type={ou.type} logo={ou.logo} />
-          </div>
-        ))}
-      </div>
-
-      {/* member details */}
-      <div className="row">
-        <h6 className="text-cl-primary text-start">Members details</h6>
-
-        {/* table fileter options */}
-        <div className="d-flex mb-2">
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Search By Name"
-            value={searchByName}
-            onChange={handleSearchChange}
-            style={{ width: "15rem" }}
-          />
-
-          {/* select entity */}
-          <select
-            className="form-control form-select ms-auto me-1"
-            value={entityFilter}
-            onChange={handleEntityChange}
-            style={{ width: "10rem" }}
-          >
-            <option value="All">Select Entity</option>
-            {ous.map((ou) => (
-              <option key={ou.id} value={ou.name}>
-                {ou.name}
-              </option>
-            ))}
-          </select>
-          {/* select term year */}
-          <select
-            className="form-control form-select ms-2 me-1"
-            value={termFilter}
-            onChange={handleTermChange}
-            style={{ width: "10rem" }}
-          >
-            <option value={currentYear}>Select Term</option>
-            {availableTermYears.map((year) => (
-              <option key={year} value={year}>
-                Term {year}
-              </option>
-            ))}
-          </select>
-        </div>
-
-        {/* excome table */}
-        <ExcomTable
-          dataList={excomData}
-          search={searchByName}
-          slectedEntity={entityFilter}
-          selectTerm={termFilter}
-        />
-      </div>
-    </div>
-  );
-};
+}
 
 export default ExcomLandingPage;
-
-const ous = [
-  {
-    id: 0,
-    name: "SB",
-    type: "Student Branch",
-    logo: sbLogo,
-  },
-  {
-    id: 1,
-    name: "WIE",
-    type: "Affinity Group",
-    logo: wieLogo,
-  },
-  {
-    id: 2,
-    name: "RAS",
-    type: "Technical Chapter",
-    logo: rasLogo,
-  },
-  {
-    id: 3,
-    name: "IAS",
-    type: "Technical Chapter",
-    logo: iasLogo,
-  },
-  {
-    id: 4,
-    name: "CS",
-    type: "Technical Chapter",
-    logo: iasLogo,
-  }
-];
-
-const excomData = [
-  {
-    id: 1,
-    fname: "Kavindra",
-    lname: "Weerasinghe",
-    email: "wdilshankavindra@gmail.com",
-    contactNo: "0774743603",
-    entity: "SB",
-    position: "Chairperson",
-    academicYear: "3rd Year",
-    termYear: "2024",
-  },
-  {
-    id: 2,
-    fname: "Sanjana",
-    lname: "Attanayake",
-    email: "sanajan@gamil.com",
-    contactNo: "0742365896",
-    entity: "SB",
-    position: "Secretary",
-    academicYear: "3rd Year",
-    termYear: "2022",
-  },
-  {
-    id: 3,
-    fname: "Kasun",
-    lname: "Janith",
-    email: "kasun@gamil.com",
-    contactNo: "0712365896",
-    entity: "IAS",
-    position: "Vice Chair",
-    academicYear: "2nd Year",
-    termYear: "2024",
-  },
-  {
-    id: 4,
-    fname: "Joshiga",
-    lname: "Ravikumar",
-    email: "joshi@gamil.com",
-    contactNo: "0712365895",
-    entity: "WIE",
-    position: "Chairperson",
-    academicYear: "3rd Year",
-    termYear: "2023",
-  },
-  {
-    id: 5,
-    fname: "Sasan",
-    lname: "Dilantha",
-    email: "sasan@gamil.com",
-    contactNo: "0712364596",
-    entity: "IAS",
-    position: "Chairperson",
-    academicYear: "3rd Year",
-    termYear: "2024",
-  },
-  {
-    id: 6,
-    fname: "Nipuna",
-    lname: "Deshan",
-    email: "nipuna@gamil.com",
-    contactNo: "0712367586",
-    entity: "IAS",
-    position: "PV Head",
-    academicYear: "3rd Year",
-    termYear: "2024",
-  },
-];
