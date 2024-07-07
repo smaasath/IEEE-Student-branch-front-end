4d3f534155adbb263ed6881ccf59192b0887428a
Thilini Priyangika <85427990+Thilinipriyangika@users.noreply.github.com>
2024-07-07 14:48:17 +0530

    Change Term Year Model

--- src/components/models/addTermYearModel/addTermYearModel.jsx
+++ src/components/models/addTermYearModel/addTermYearModel.jsx
@@ -29,11 +29,12 @@ const TermYearModel = ({ onHide, show, disabled, editable, id }) => {
               <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="Enrolled Year" disabled={disabled} />
             </div>
           </div>
-          <div className='mt-3'>
-            <div className="form-group">
-              <label htmlFor="exampleFormControlTextarea1">Description</label>
-              <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" disabled={disabled}></textarea>
-            </div>
+          <div className="mt-3">
+            <label htmlFor="exampleFormControlInput1" className="form-label text-dark">Status</label>
+            <select className="form-select w-100" aria-label="Large select example" disabled={disabled}>
+              <option>Select Status</option>
+              <option value="1">Active</option>
+            </select>
           </div>
         </div>
       </Modal.Body>
