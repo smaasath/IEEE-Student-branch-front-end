fbaf94c7b78eb53e3236e6435993616c97246a92
Aasath <aasadh2000@gmail.com>
2024-07-07 14:12:15 +0530

    get academic year

--- src/components/common/commonSearch/commonSearch.jsx
+++ src/components/common/commonSearch/commonSearch.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import search from '../../../assets/icons/search.png'
 
 
-const CommonSearch = ({primary}) => {
+const CommonSearch = ({primary,onChange}) => {
     return (
         <div>
             <div className="input-group">
-                <input type="search" className="form-control form-control-sm" placeholder="Search....." aria-label="Recipient's username" aria-describedby="basic-addon2" />
+                <input onChange={(item)=>onChange(item)} type="search" className="form-control form-control-sm" placeholder="Search....." aria-label="Recipient's username" aria-describedby="basic-addon2" />
                 <div className={`input-group-append ps-3 pe-3 p-2 text-center rounded-end-3 ${primary ? "bag-primary" : "bg-third"}`}>
                     <img style={{ width: 15, height: 15 }} src={search} className='img-fluid' />
                 </div>
