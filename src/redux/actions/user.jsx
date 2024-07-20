cf24f9583dc6ef1463de32d53ab40f05380a44e9
WDK-Techno <wdilshankavindra@gmail.com>
2024-07-20 09:42:00 +0530

    singup page

--- src/redux/actions/user.jsx
+++ src/redux/actions/user.jsx
@@ -3,33 +3,58 @@ import Cookies from "js-cookie";
 
 const http = new HttpInterceptor();
 
-export const userLogin = (body, callback)  => (dispatch) =>  {
-    const endpoint = `${import.meta.env.VITE_API_HOST}auth/authenticate`;
-  
-    try {
-      http
-        .post(endpoint, body)
-        .then((response) => {
-            callback(response);
-            if (response.status === 200) {
-                const cookieOptions = {
-                  path: "/",
-                };
-                console.warn("cookie")
-                Cookies.set(
-                  "token",
-                  response?.data?.data?.access_token,
-                  cookieOptions
-                );
-              }
+export const userLogin = (body, callback) => (dispatch) => {
+  const endpoint = `${import.meta.env.VITE_API_HOST}auth/authenticate`;
 
-              
-    
-        })
-        .catch((error) => {
-          callback(error.response);
-        });
-    } catch (error) {
-      callback(error.response);
-    }
-  };
+  try {
+    http
+      .post(endpoint, body)
+      .then((response) => {
+        callback(response);
+        if (response.status === 200) {
+          const cookieOptions = {
+            path: "/",
+          };
+          console.warn("cookie");
+          Cookies.set(
+            "token",
+            response?.data?.data?.access_token,
+            cookieOptions
+          );
+        }
+      })
+      .catch((error) => {
+        callback(error.response);
+      });
+  } catch (error) {
+    callback(error.response);
+  }
+};
+
+export const userRegistration = (body, callback) => (dispatch) => {
+  const endpoint = `${import.meta.env.VITE_API_HOST}auth/register`;
+
+  try {
+    http
+      .post(endpoint, body)
+      .then((response) => {
+        callback(response);
+        if (response.status === 200) {
+          const cookieOptions = {
+            path: "/",
+          };
+          console.warn("cookie");
+          Cookies.set(
+            "token",
+            response?.data?.data?.access_token,
+            cookieOptions
+          );
+        }
+      })
+      .catch((error) => {
+        callback(error.response);
+      });
+  } catch (error) {
+    callback(error.response);
+  }
+};
