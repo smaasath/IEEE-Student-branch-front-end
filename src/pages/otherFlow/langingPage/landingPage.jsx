ca290f22be948be00920100bb761e2473d12e4eb
Aasath <aasadh2000@gmail.com>
2024-08-03 19:24:43 +0530

    other policy added

--- src/pages/otherFlow/langingPage/landingPage.jsx
+++ src/pages/otherFlow/langingPage/landingPage.jsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import termyear from "../../../assets/images/termyear.png";
 import policy from "../../../assets/images/policy.png";
 import userrole from "../../../assets/images/userrole.png"
+import { useSelector } from "react-redux";
+import CommonLoader from "../../../components/common/commonLoader/commonLoader";
 
 const OtherLandingPage = () => {
+  const userData = useSelector((state) => state.user.userData);
   const navigate = useNavigate();
+  const [pageLoading, setPageLoading] = useState(true);
+  useEffect(() => {
+    setPageLoading(true)
+    if (userData) {
+      const isOtherAvailable = userData?.role?.some(role =>
+        role.policies.some(policy => policy.policyCode === "OTHER")
+      );
+      if (!isOtherAvailable) {
+        navigate('/dashboard')
+      } else {
+        setPageLoading(false);
+      }
+    }
+  }, [userData])
+
   const navigateTotermYearPage = () => {
     navigate("/dashboard/other/academic-year");
   };
@@ -17,44 +35,51 @@ const OtherLandingPage = () => {
   };
 
   return (
-    <div className="mt-2 d-flex justify-content-between align-items-center gap-4 flex-wrap'">
-      <button
-        onClick={() => {
-          navigateTotermYearPage();
-        }}
-        className="bg-white border-0 rounded-4 common-transition common-shadow d-flex justify-content-between align-items-center p-3"
-        style={{ width: 350 }}
-      >
-        <div className="h4 fw-bold text-cl-primary">Term Year</div>
-        <div>
-          <img src={termyear} width={70} />
+    <>
+      {pageLoading ? (
+        <CommonLoader />
+      ) : (
+        <div className="mt-2 d-flex justify-content-between align-items-center gap-4 flex-wrap'">
+          <button
+            onClick={() => {
+              navigateTotermYearPage();
+            }}
+            className="bg-white border-0 rounded-4 common-transition common-shadow d-flex justify-content-between align-items-center p-3"
+            style={{ width: 350 }}
+          >
+            <div className="h4 fw-bold text-cl-primary">Term Year</div>
+            <div>
+              <img src={termyear} width={70} />
+            </div>
+          </button>
+          <button
+            onClick={() => {
+              navigateTopolicyPage();
+            }}
+            className="bg-white border-0 rounded-4 common-transition common-shadow d-flex justify-content-between align-items-center p-3"
+            style={{ width: 350 }}
+          >
+            <div className="h4 fw-bold text-cl-primary">Policy</div>
+            <div>
+              <img src={policy} width={70} />
+            </div>
+          </button>
+          <button
+            onClick={() => {
+              navigateTouserRolePage();
+            }}
+            className="bg-white border-0 rounded-4 common-transition common-shadow d-flex justify-content-between align-items-center p-3"
+            style={{ width: 350 }}
+          >
+            <div className="h4 fw-bold text-cl-primary">User Roles</div>
+            <div>
+              <img src={userrole} width={70} />
+            </div>
+          </button>
         </div>
-      </button>
-      <button
-        onClick={() => {
-          navigateTopolicyPage();
-        }}
-        className="bg-white border-0 rounded-4 common-transition common-shadow d-flex justify-content-between align-items-center p-3"
-        style={{ width: 350 }}
-      >
-        <div className="h4 fw-bold text-cl-primary">Policy</div>
-        <div>
-          <img src={policy} width={70} />
-        </div>
-      </button>
-      <button
-        onClick={() => {
-          navigateTouserRolePage();
-        }}
-        className="bg-white border-0 rounded-4 common-transition common-shadow d-flex justify-content-between align-items-center p-3"
-        style={{ width: 350 }}
-      >
-        <div className="h4 fw-bold text-cl-primary">User Roles</div>
-        <div>
-          <img src={userrole} width={70} />
-        </div>
-      </button>
-    </div>
+      )}
+    </>
+
   );
 };
 
--- src/pages/otherFlow/userRolePage/userRolePage.jsx
+++ src/pages/otherFlow/userRolePage/userRolePage.jsx
@@ -4,13 +4,15 @@ import CommonTable from '../../../components/common/commonTable/commonTable';
 import CommonPagination from '../../../components/common/commonPagination/commonPagination';
 import EditPrimary from '../../../assets/icons/editPrimary.png';
 import CommonButton from '../../../components/common/commonButton/commonButton';
-import UserRoleModel from '../../../components/models/addUserRoleModel/addUserRoleModel'; 
+import UserRoleModel from '../../../components/models/addUserRoleModel/addUserRoleModel';
 import { getAllRoles } from '../../../redux/actions/role';
-
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import CommonLoader from '../../../components/common/commonLoader/commonLoader';
 
 const UserRolePage = () => {
   const [currentPage, setCurrentPage] = useState(1);
-  const [userRoleModelShow, setUserRoleModelShow] = useState(false); 
+  const [userRoleModelShow, setUserRoleModelShow] = useState(false);
   const [editable, setEditable] = useState(false);
   const [disable, setDisable] = useState(false);
   const [item, setitem] = useState(null);
@@ -19,16 +21,31 @@ const UserRolePage = () => {
   const [totalPage, setTotalPage] = useState(1);
   const [status, setStatus] = useState('');
   const [searchItem, setsearchItem] = useState('');
-  const [refreshTable , setRefreshTable] = useState(0)
-
+  const [refreshTable, setRefreshTable] = useState(0)
+  const userData = useSelector((state) => state.user.userData);
+  const navigate = useNavigate();
+  const [pageLoading, setPageLoading] = useState(true);
+  useEffect(() => {
+    setPageLoading(true)
+    if (userData) {
+      const isOtherAvailable = userData?.role?.some(role =>
+        role.policies.some(policy => policy.policyCode === "OTHER")
+      );
+      if (!isOtherAvailable) {
+        navigate('/dashboard')
+      } else {
+        setPageLoading(false);
+      }
+    }
+  }, [userData])
   const tableHeading = [
     { label: "ID", value: "id" },
     { label: "Role", value: "userRole" },
     { label: "Type", value: "type" },
-    { label: "", value: "ACTION" ,type: ["EDIT"]},
+    { label: "", value: "ACTION", type: ["EDIT"] },
   ];
 
-  function search(item){
+  function search(item) {
     setsearchItem(item?.target?.value)
     console.warn(item?.target?.value);
   }
@@ -69,34 +86,40 @@ const UserRolePage = () => {
 
   return (
     <>
-      <div className='mt-4 d-flex justify-content-end'><div><CommonButton onClick={() => setUserRoleModelShow(true)} text={"Add"} /></div></div>
-      <div className='mt-3 pt-4 p-3 rounded-4 bg-white common-shadow'>
-        <div className='d-flex justify-content-between flex-wrap align-items-center p-3'>
-          <CommonSearch primary={true} onChange={(item)=>{search(item)}} />
-        </div>
-        <div className='mt-3 p-3 rounded-4 bg-white d-flex flex-column justify-content-between table-container'>
-          <CommonTable
-            tableHeading={tableHeading}
-            tableData={roleData}
-            primary={true}
-            loading={loader}
-            editAction={(item) => { editRole(item) }}
-          />
-          <div className='mt-4 d-flex justify-content-end'>
-            <CommonPagination pages={totalPage} currentPage={currentPage} setCurrentPage={setCurrentPage} />
+      {pageLoading ? (
+        <CommonLoader />
+      ) : (
+        <>
+          <div className='mt-4 d-flex justify-content-end'><div><CommonButton onClick={() => setUserRoleModelShow(true)} text={"Add"} /></div></div>
+          <div className='mt-3 pt-4 p-3 rounded-4 bg-white common-shadow'>
+            <div className='d-flex justify-content-between flex-wrap align-items-center p-3'>
+              <CommonSearch primary={true} onChange={(item) => { search(item) }} />
+            </div>
+            <div className='mt-3 p-3 rounded-4 bg-white d-flex flex-column justify-content-between table-container'>
+              <CommonTable
+                tableHeading={tableHeading}
+                tableData={roleData}
+                primary={true}
+                loading={loader}
+                editAction={(item) => { editRole(item) }}
+              />
+              <div className='mt-4 d-flex justify-content-end'>
+                <CommonPagination pages={totalPage} currentPage={currentPage} setCurrentPage={setCurrentPage} />
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-      <UserRoleModel
-        show={userRoleModelShow}
-        onHide={handleCloseUserRoleModel}
-        editable={editable}
-        disabled={disable}
-        selectedMember={null} 
-        item={item}
-        changed={()=>{setRefreshTable(refreshTable+1)}}
+          <UserRoleModel
+            show={userRoleModelShow}
+            onHide={handleCloseUserRoleModel}
+            editable={editable}
+            disabled={disable}
+            selectedMember={null}
+            item={item}
+            changed={() => { setRefreshTable(refreshTable + 1) }}
 
-      />
+          />
+        </>
+      )}
     </>
   );
 };
