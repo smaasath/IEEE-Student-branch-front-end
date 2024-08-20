6a4f5b957ce99efdaa0c1ab47d5dbd82539d2851
WDK-Techno <wdilshankavindra@gmail.com>
2024-08-20 07:13:48 +0530

    Change Set Prority in Other Flow -> Role Table & Add Role Model

--- src/components/models/addUserRoleModel/addUserRoleModel.jsx
+++ src/components/models/addUserRoleModel/addUserRoleModel.jsx
@@ -1,32 +1,38 @@
-import React, { useEffect, useState } from 'react';
-import Modal from 'react-bootstrap/Modal';
-import CommonButton from '../../common/commonButton/commonButton';
-import CommonSearch from '../../common/commonSearch/commonSearch';
-import { createRole, updateRole } from '../../../redux/actions/role';
-import { assignPolicy, getAllPolicy } from '../../../redux/actions/policy';
+import React, { useEffect, useState } from "react";
+import Modal from "react-bootstrap/Modal";
+import CommonButton from "../../common/commonButton/commonButton";
+import CommonSearch from "../../common/commonSearch/commonSearch";
+import { createRole, updateRole } from "../../../redux/actions/role";
+import { assignPolicy, getAllPolicy } from "../../../redux/actions/policy";
 
 const UserRoleModel = ({ onHide, show, editable, disabled, item, changed }) => {
   const [formData, setFormData] = useState({
     userRole: "",
     type: "",
+    priorityMain: "",
+    prioritySub: "",
   });
 
   const [error, setError] = useState({
     userRole: false,
     type: false,
+    priorityMain: false,
+    prioritySub: false,
   });
 
-  const [policies, setPolicies] = useState(null)
-  const [selectError, SetSelectError] = useState('')
+  const [policies, setPolicies] = useState(null);
+  const [selectError, SetSelectError] = useState("");
   const [loading, setLoading] = useState(false);
-  const [exist, setExist] = useState('');
-  const [type, setType] = useState('');
-  const [searchItem, setsearchItem] = useState('');
+  const [exist, setExist] = useState("");
+  const [type, setType] = useState("");
+  const [searchItem, setsearchItem] = useState("");
 
   const [selectedPolicy, setSelectedPolicy] = useState([]);
 
   function onSelectItem(data) {
-    const index = selectedPolicy.findIndex((item) => item.policyID === data.policyID);
+    const index = selectedPolicy.findIndex(
+      (item) => item.policyID === data.policyID
+    );
     if (index === -1) {
       setSelectedPolicy([...selectedPolicy, data]);
     } else {
@@ -41,144 +47,137 @@ const UserRoleModel = ({ onHide, show, editable, disabled, item, changed }) => {
   }
 
   function search(item) {
-    setsearchItem(item?.target?.value)
+    setsearchItem(item?.target?.value);
     console.warn(item?.target?.value);
   }
 
   useEffect(() => {
-    console.warn(item, "itemmm")
+    console.warn(item, "itemmm");
 
     getAllPolicy(0, searchItem, type, (res) => {
       if (res.status == 200) {
-        setPolicies(res?.data?.data?.content)
-
+        setPolicies(res?.data?.data?.content);
       }
-
-    })
-  }, [searchItem, type])
+    });
+  }, [searchItem, type]);
 
   useEffect(() => {
     if (editable && show == true) {
-      setSelectedPolicy(item.policies)
+      setSelectedPolicy(item.policies);
     }
-
-  }, [show])
+  }, [show]);
 
   useEffect(() => {
-
     if (!editable) {
       setFormData({
         userRole: "",
         type: "",
+        priorityMain: "",
+        prioritySub: "",
       });
     } else {
-      setFormData(item)
+      setFormData(item);
     }
 
     setError({
       userRole: false,
       type: false,
+      priorityMain: false,
+      prioritySub: false,
     });
-    SetSelectError('')
-    setExist('')
-
-  }, [show])
-
-
+    SetSelectError("");
+    setExist("");
+  }, [show, editable, item]);
 
   const handleTypeChange = (e) => {
     const { value } = e.target;
-    setType(value)
+    setType(value);
   };
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setFormData((prevData) => ({ ...prevData, [name]: value }));
     setError((prevError) => ({ ...prevError, [name]: false }));
-    setExist('')
-    SetSelectError('')
+    setExist("");
+    SetSelectError("");
   };
-
 
   function addRole() {
     const idArray = selectedPolicy.map(({ policyID }) => policyID);
-    setExist('')
+    setExist("");
     setError({
       userRole: false,
       type: false,
     });
 
-    if (!formData.userRole || formData.type == '') {
+    if (!formData.userRole || formData.type == "" || formData.priorityMain == "" || formData.prioritySub == "") {
       setError({
         ...error,
         userRole: !formData.userRole,
-        type: formData.type == '' ? true : false,
+        type: formData.type == "" ? true : false,
+        priorityMain: formData.priorityMain == "" ? true : false,
+        prioritySub: formData.prioritySub == "" ? true : false,
       });
       return;
     }
 
     if (selectedPolicy.length > 0) {
-      setLoading(true)
+      setLoading(true);
       if (editable) {
         formData.roleID = item?.id;
         updateRole(formData, (res) => {
-          console.log(res.data)
+          console.log(res.data);
           if (res?.status == 200) {
-            assignPolicy({
-              roleId: res?.data?.data?.roleID,
-              policies: idArray
-            }, (res) => {
-              if (res.status == 200) {
-                console.warn(res)
-                setLoading(false)
-                changed()
-                onHide()
+            assignPolicy(
+              {
+                roleId: res?.data?.data?.roleID,
+                policies: idArray,
+              },
+              (res) => {
+                if (res.status == 200) {
+                  console.warn(res);
+                  setLoading(false);
+                  changed();
+                  onHide();
+                }
               }
-
-            })
-
+            );
           } else {
-            setLoading(false)
-            setExist("Role Edited Failed")
+            setLoading(false);
+            setExist("Role Edited Failed");
           }
-        })
+        });
       } else {
-
         createRole(formData, (res) => {
           if (res?.status == 201) {
-            console.warn()
+            console.warn();
 
-            assignPolicy({
-              roleId: res?.data?.data?.roleID,
-              policies: idArray
-            }, (res) => {
-              if (res.status == 200) {
-                console.warn(res)
-                setLoading(false)
-                changed()
-                onHide()
+            assignPolicy(
+              {
+                roleId: res?.data?.data?.roleID,
+                policies: idArray,
+              },
+              (res) => {
+                if (res.status == 200) {
+                  console.warn(res);
+                  setLoading(false);
+                  changed();
+                  onHide();
+                }
               }
-
-            })
-
-
-
+            );
           } else if (res?.status == 409) {
-            setLoading(false)
-            setExist("Role Already Exist")
+            setLoading(false);
+            setExist("Role Already Exist");
           } else {
-            setLoading(false)
-            setExist("Role Added Failed")
+            setLoading(false);
+            setExist("Role Added Failed");
           }
-
-
-        })
+        });
       }
-
     } else {
-      SetSelectError("Please Select a Policy")
+      SetSelectError("Please Select a Policy");
     }
-
   }
 
   return (
@@ -192,41 +191,129 @@ const UserRoleModel = ({ onHide, show, editable, disabled, item, changed }) => {
         centered
       >
         <Modal.Header closeButton>
-          <Modal.Title className='text-cl-primary' id="contained-modal-title-vcenter">
+          <Modal.Title
+            className="text-cl-primary"
+            id="contained-modal-title-vcenter"
+          >
             {editable ? "Edit" : disabled ? "View" : "Add"}
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <div className='d-flex flex-column'>
-            <div className='mt-3'>
+          <div className="d-flex flex-column">
+            <div className="mt-3">
               <div className="">
-                <label htmlFor="roleInput" className="form-label text-dark">Role Name</label>
-                <input type="text" className={`form-control ${error.userRole ? "is-invalid" : ""}`} name='userRole' value={formData.userRole} onChange={handleInputChange} id="roleInput" placeholder="Role Name" disabled={disabled} />
-                <div class="invalid-feedback">
-                  This field is required.
-                </div>
+                <label htmlFor="roleInput" className="form-label text-dark">
+                  Role Name
+                </label>
+                <input
+                  type="text"
+                  className={`form-control ${
+                    error.userRole ? "is-invalid" : ""
+                  }`}
+                  name="userRole"
+                  value={formData.userRole}
+                  onChange={handleInputChange}
+                  id="roleInput"
+                  placeholder="Role Name"
+                  disabled={disabled}
+                />
+                <div className="invalid-feedback">This field is required.</div>
               </div>
             </div>
             <div className="mt-3">
-              <label htmlFor="typeSelect" className="form-label text-dark">Type</label>
-              <select value={formData.type} name='type' onChange={handleInputChange} className={`form-select w-100 ${error.type ? "is-invalid" : ""}`} aria-label="Large select example" disabled={disabled}>
-                <option value=''>Select Type</option>
+              <label htmlFor="typeSelect" className="form-label text-dark">
+                Type
+              </label>
+              <select
+                value={formData.type}
+                name="type"
+                onChange={handleInputChange}
+                className={`form-select w-100 ${
+                  error.type ? "is-invalid" : ""
+                }`}
+                aria-label="Large select example"
+                disabled={disabled}
+              >
+                <option value="">Select Type</option>
                 <option value="MAIN">Main</option>
                 <option value="EXCOM">Excom</option>
                 <option value="PROJECT">Project</option>
               </select>
-              <div class="invalid-feedback">
-                This field is required.
-              </div>
+              <div className="invalid-feedback">This field is required.</div>
             </div>
 
-            <div className='mt-5'>
-            <label htmlFor="roleInput" className="form-label text-dark">Policy</label>
-              <div className='d-flex justify-content-between'>
-                <CommonSearch onChange={(item) => { search(item) }} />
+            <div className="mt-3">
+              <label
+                htmlFor="priorityMainSelect"
+                className="form-label text-dark"
+              >
+                Main Priority
+              </label>
+              <select
+                value={formData.priorityMain}
+                name="priorityMain"
+                onChange={handleInputChange}
+                className={`form-select w-100 ${
+                  error.priorityMain ? "is-invalid" : ""
+                }`}
+                aria-label="Large select example"
+                disabled={disabled}
+              >
+                <option value="">Select Main Priority</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+              </select>
+              <div className="invalid-feedback">This field is required.</div>
+            </div>
+
+            <div className="mt-3">
+              <label
+                htmlFor="prioritySubSelect"
+                className="form-label text-dark"
+              >
+                Sub Priority
+              </label>
+              <select
+                value={formData.prioritySub}
+                name="prioritySub"
+                onChange={handleInputChange}
+                className={`form-select w-100 ${
+                  error.prioritySub ? "is-invalid" : ""
+                }`}
+                aria-label="Large select example"
+                disabled={disabled}
+              >
+                <option value="">Select Sub Priority</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+              </select>
+              <div className="invalid-feedback">This field is required.</div>
+            </div>
+            <div className="mt-5">
+              <label htmlFor="roleInput" className="form-label text-dark">
+                Policy
+              </label>
+              <div className="d-flex justify-content-between">
+                <CommonSearch
+                  onChange={(item) => {
+                    search(item);
+                  }}
+                />
                 <div>
-                  <select name='type' onChange={handleTypeChange} className={`form-select`} aria-label="Large select example">
-                    <option value=''>Select Type</option>
+                  <select
+                    name="type"
+                    onChange={handleTypeChange}
+                    className={`form-select`}
+                    aria-label="Large select example"
+                  >
+                    <option value="">Select Type</option>
                     <option value="MAIN">Main</option>
                     <option value="EXCOM">Excom</option>
                     <option value="PROJECT">Project</option>
@@ -234,9 +321,12 @@ const UserRoleModel = ({ onHide, show, editable, disabled, item, changed }) => {
                 </div>
               </div>
 
-              <div className="table-responsive mt-2 overflow-scroll" style={{ maxHeight: 300 }}>
+              <div
+                className="table-responsive mt-2 overflow-scroll"
+                style={{ maxHeight: 300 }}
+              >
                 <table className="table table-bordered">
-                  <thead className='sticky-top z-1'>
+                  <thead className="sticky-top z-1">
                     <tr>
                       <th scope="col"></th>
                       <th scope="col">Policy</th>
@@ -261,25 +351,23 @@ const UserRoleModel = ({ onHide, show, editable, disabled, item, changed }) => {
                   </tbody>
                 </table>
               </div>
-              <div className='text-danger text-center mt-4'>
-                {selectError}
-              </div>
+              <div className="text-danger text-center mt-4">{selectError}</div>
             </div>
           </div>
         </Modal.Body>
-        <Modal.Footer className='d-flex justify-content-end'>
+        <Modal.Footer className="d-flex justify-content-end">
           <div>
             <CommonButton onClick={onHide} close={true} text={"Close"} />
           </div>
-          {
-            disabled ? null : (
-              <div>
-                <CommonButton load={loading} onClick={addRole} text={editable ? "Save" : "Add"} />
-              </div>
-            )
-          }
-
-
+          {disabled ? null : (
+            <div>
+              <CommonButton
+                load={loading}
+                onClick={addRole}
+                text={editable ? "Save" : "Add"}
+              />
+            </div>
+          )}
         </Modal.Footer>
       </Modal>
     </>
