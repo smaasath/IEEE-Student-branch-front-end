c8ab73d30c9de27a6009b2aa4384f312f68fca33
Aasath <aasadh2000@gmail.com>
2024-10-11 13:57:10 +0530

    loader

--- src/pages/projectFlow/projectLandingPage/projectLandingPage.jsx
+++ src/pages/projectFlow/projectLandingPage/projectLandingPage.jsx
@@ -8,6 +8,7 @@ import CommonButton from '../../../components/common/commonButton/commonButton'
 import { useNavigate } from 'react-router-dom'
 import ProjectModel from '../../../components/models/projectModel/projectModel'
 import { useSelector } from 'react-redux'
+import CommonLoader from '../../../components/common/commonLoader/commonLoader'
 
 const ProjectLandingPage = () => {
     const [currentPage, setCurrentPage] = useState(1);
@@ -17,6 +18,8 @@ const ProjectLandingPage = () => {
     const [id, setId] = useState(null);
     const userData = useSelector((state) => state.user.userData);
     const [projectPolicy, setProjectPolicy] = useState(false);
+    const [pageLoading, setPageLoading] = useState(true);
+
 
     useEffect(() => {
         setPageLoading(true);
@@ -28,6 +31,7 @@ const ProjectLandingPage = () => {
             );
 
             setProjectPolicy(isProjectAvailable);
+            setPageLoading(false)
         }
     }, [userData]);
 
@@ -108,74 +112,80 @@ const ProjectLandingPage = () => {
 
     return (
         <>
-            <div>
-                <div className='d-flex justify-content-end gap-3 flex-wrap align-items-center'>
-                    <div className="">
-                        <select className="form-select w-100" aria-label="Large select example">
-                            <option selected>Select Entity</option>
-                            <option value="1">SB</option>
-                            <option value="1">WIE</option>
-                            <option value="1">RAS</option>
-                            <option value="1">IAS</option>
-                            <option value="1">CS</option>
-                        </select>
-                    </div>
+            {pageLoading ? (
+                <CommonLoader />
+            ) : (
+                <>
+                    <div>
+                        <div className='d-flex justify-content-end gap-3 flex-wrap align-items-center'>
+                            <div className="">
+                                <select className="form-select w-100" aria-label="Large select example">
+                                    <option selected>Select Entity</option>
+                                    <option value="1">SB</option>
+                                    <option value="1">WIE</option>
+                                    <option value="1">RAS</option>
+                                    <option value="1">IAS</option>
+                                    <option value="1">CS</option>
+                                </select>
+                            </div>
 
-                    <div className="">
-                        <select className="form-select w-100" aria-label="Large select example">
-                            <option selected>Select Year</option>
-                            <option value="1">2024</option>
-                            <option value="1">2023</option>
-                            <option value="1">2022</option>
-                        </select>
-                    </div>
-                </div>
-                <div className='text-cl-primary'>Project</div>
-                <div className='mt-2 d-flex justify-content-between align-items-center gap-4 flex-wrap'>
-                    <div className='d-flex justify-content-between gap-4 rounded-4 bg-body-secondary p-4 flex-wrap flex-grow-1'>
-                        <CommonStatusCountCard type={"TODO"} count={1} />
-                        <CommonStatusCountCard type={"ONGOING"} count={1} />
-                        <CommonStatusCountCard type={"COMPLETE"} count={1} />
-                    </div>
-                    <button onClick={() => { navigateToTimeLine() }} className='bg-white border-0 rounded-4 common-transition common-shadow d-flex justify-content-between align-items-center p-3' style={{ width: 350 }}>
-                        <div className='h4 fw-bold text-cl-primary'>
-                            Projects  time line
+                            <div className="">
+                                <select className="form-select w-100" aria-label="Large select example">
+                                    <option selected>Select Year</option>
+                                    <option value="1">2024</option>
+                                    <option value="1">2023</option>
+                                    <option value="1">2022</option>
+                                </select>
+                            </div>
                         </div>
-                        <div>
-                            <img src={timeLinefrom} width={70} />
+                        <div className='text-cl-primary'>Project</div>
+                        <div className='mt-2 d-flex justify-content-between align-items-center gap-4 flex-wrap'>
+                            <div className='d-flex justify-content-between gap-4 rounded-4 bg-body-secondary p-4 flex-wrap flex-grow-1'>
+                                <CommonStatusCountCard type={"TODO"} count={1} />
+                                <CommonStatusCountCard type={"ONGOING"} count={1} />
+                                <CommonStatusCountCard type={"COMPLETE"} count={1} />
+                            </div>
+                            <button onClick={() => { navigateToTimeLine() }} className='bg-white border-0 rounded-4 common-transition common-shadow d-flex justify-content-between align-items-center p-3' style={{ width: 350 }}>
+                                <div className='h4 fw-bold text-cl-primary'>
+                                    Projects  time line
+                                </div>
+                                <div>
+                                    <img src={timeLinefrom} width={70} />
+                                </div>
+                            </button>
                         </div>
-                    </button>
-                </div>
 
-                {
-                    projectPolicy ? (
-                        <div className='mt-4 d-flex justify-content-end'><div><CommonButton onClick={handleShowProjectModel} text={"Add Project"} /></div></div>
-                    ) : null
-                }
+                        {
+                            projectPolicy ? (
+                                <div className='mt-4 d-flex justify-content-end'><div><CommonButton onClick={handleShowProjectModel} text={"Add Project"} /></div></div>
+                            ) : null
+                        }
 
-                <div className='mt-4 d-flex flex-column gap-3 justify-content-center bg-white rounded-2 common-shadow p-3'>
-                    <div className='mt-2 d-flex flex-wrap justify-content-between align-items-center'>
-                        <CommonSearch primary={true} />
-                        <div className="">
-                            <select className="form-select w-100" aria-label="Large select example">
-                                <option selected>Select Status</option>
-                                <option value="TODO">To Do</option>
-                                <option value="ONGOING">Ongoing</option>
-                                <option value="COMPLETE">Complete</option>
-                            </select>
+                        <div className='mt-4 d-flex flex-column gap-3 justify-content-center bg-white rounded-2 common-shadow p-3'>
+                            <div className='mt-2 d-flex flex-wrap justify-content-between align-items-center'>
+                                <CommonSearch primary={true} />
+                                <div className="">
+                                    <select className="form-select w-100" aria-label="Large select example">
+                                        <option selected>Select Status</option>
+                                        <option value="TODO">To Do</option>
+                                        <option value="ONGOING">Ongoing</option>
+                                        <option value="COMPLETE">Complete</option>
+                                    </select>
+                                </div>
+                            </div>
+
+                            <div className='mt-4 table-container'>
+                                <CommonTable tableHeading={tableHeading} primary={true} tableData={tableData} loading={false} viewAction={(id) => { navigateToProject(id) }} editAction={(id) => { editProject(id) }} />
+                            </div>
+                            <div className='mt-4 d-flex justify-content-end'>
+                                <CommonPagination pages={10} currentPage={currentPage} setCurrentPage={setCurrentPage} />
+                            </div>
                         </div>
                     </div>
 
-                    <div className='mt-4 table-container'>
-                        <CommonTable tableHeading={tableHeading} primary={true} tableData={tableData} loading={false} viewAction={(id) => { navigateToProject(id) }} editAction={(id) => { editProject(id) }} />
-                    </div>
-                    <div className='mt-4 d-flex justify-content-end'>
-                        <CommonPagination pages={10} currentPage={currentPage} setCurrentPage={setCurrentPage} />
-                    </div>
-                </div>
-            </div>
-
-            <ProjectModel show={projectModelShow} onHide={handleCloseProjectModel} disabled={disable} editable={editable} id={id} />
+                    <ProjectModel show={projectModelShow} onHide={handleCloseProjectModel} disabled={disable} editable={editable} id={id} />
+                </>
+            )}
         </>
     )
 }
